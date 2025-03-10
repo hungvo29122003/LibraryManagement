7fa5f471af027501aa60fcdf02f5e56fe837be05
hungvo29122003 <acczinsv5@gmail.com>
2025-03-10 16:24:17 +0700

    update

---
+++ app/src/main/java/com/example/librarymanagement/item_bao_cao.kt
@@ -0,0 +1,20 @@
+package com.example.librarymanagement
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class item_bao_cao : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_item_bao_cao)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
